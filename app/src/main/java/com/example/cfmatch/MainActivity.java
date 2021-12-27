3365c180e2665ee979c732ed907ebec5fc7d5f78
Gabriel Leite <leitedasilvag@gmail.com>
2021-12-27 16:26:35 -0300

    feat: implement pages of register and signIn

--- app/src/main/java/com/example/cfmatch/MainActivity.java
+++ app/src/main/java/com/example/cfmatch/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.cfmatch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void submit(View c) {
+        Intent i = new Intent(getBaseContext(), login.class);
+
+        startActivity(i);
     }
 }
