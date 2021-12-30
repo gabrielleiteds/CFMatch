73f13574bda79ba37e0e3b0d68af453d5d836826
Caduzera <carloseduardodiasm@gmail.com>
2021-12-29 21:27:40 -0300

    aaa

--- app/src/main/java/com/example/cfmatch/Matches.java
+++ app/src/main/java/com/example/cfmatch/Matches.java
@@ -6,15 +6,21 @@ import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.example.cfmatch.entities.User;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Matches extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_matches);
     }
+
+    private List<User> getMacthedUsers () {
+        return null;
+    }
+
 }
