cd6aa491a73c01f27b1f9f90d7010d99e7fe6d08
Gabriel Leite <leitedasilvag@gmail.com>
2021-12-31 01:44:19 -0300

    list interests

--- app/src/main/java/com/example/cfmatch/UserProfile.java
+++ app/src/main/java/com/example/cfmatch/UserProfile.java
@@ -7,24 +7,32 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import com.example.cfmatch.dao.InterestDao;
+import com.example.cfmatch.dao.InterestUserDao;
 import com.example.cfmatch.dao.UserDao;
+import com.example.cfmatch.entities.Interest;
 import com.example.cfmatch.entities.User;
+import com.example.cfmatch.entities.UserInterest;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserProfile extends AppCompatActivity {
 
     private int userId;
     private User user;
-    private TextView username, email, description;
+    private TextView username, email, description, interests;
     private UserDao userDao;
-
+    private InterestDao interestDao;
+    private InterestUserDao userInterestDao;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
 
         userDao = new UserDao(this);
+        userInterestDao = new InterestUserDao(this);
+        interestDao = new InterestDao(this);
 
         Intent i = getIntent();
         userId = i.getIntExtra("userId", 0);
@@ -32,6 +40,7 @@ public class UserProfile extends AppCompatActivity {
         username = findViewById(R.id.name);
         email = findViewById(R.id.email);
         description = findViewById(R.id.description);
+        interests = findViewById(R.id.interests);
 
         adjustInfos();
     }
@@ -44,6 +53,27 @@ public class UserProfile extends AppCompatActivity {
                 description.setText(user.description);
                 email.setText(user.email);
                 username.setText(user.name);
+
+                List<UserInterest> userInterests = userInterestDao.getAll();
+                List<Interest> interestsList = interestDao.getAll();
+                List<String> interestUserList = new ArrayList<>();
+
+                for(UserInterest userInterest : userInterests) {
+                    if(userInterest.userId == userId) {
+                        for (Interest interest : interestsList) {
+                            if(interest.id == userInterest.interestId) {
+                                interestUserList.add(interest.title);
+                            }
+                        }
+                    }
+                }
+
+                String text = "";
+                for(String title : interestUserList) {
+                    text += title + ", ";
+                    interests.setText(text);
+                }
+
             }
         }
     }
